07c07ee469503f9a2da8560c5d11484cb940ce9a
Anmolpreet Kaur <anmolpreet.kaur@yale.edu>
2020-05-12 20:41:19 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -82,7 +82,10 @@ def adding_matz
       }
     }
 programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
-programmer_hash[:yukihiro_matsumoto][:languages] = ["Lisp", "C"]
+programmer_hash[:yukihiro_matsumoto][:languages] = []
+programmer_hash[:yukihiro_matsumoto][:languages] << "LISP"
+programmer_hash[:yukihiro_matsumoto][:languages] << "C"
+puts programmer_hash
 end
 
 def changing_alan
